957508089a71771a9b6690e02a2e1ded52d432a1
42hena <somnium1108@gmail.com>
2022-09-26 20:10:49 +0900

    Feat : Define Header

---
+++ Define.hpp
@@ -0,0 +1,10 @@
+#ifndef __DEFINE_HPP__
+#define __DEFINE_HPP__
+
+#define VECTOR  std::vector
+#define MAP     std::map
+#define STRING  std::string
+#define CRLF    "\r\n"
+#define LF      "\n"
+
+#endif
