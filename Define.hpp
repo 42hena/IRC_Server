58f2f0e02bdafd70f48c3c70e727e4fa488335c8
42hena <somnium1108@gmail.com>
2022-09-26 20:51:28 +0900

    Refactor : Define header

--- Define.hpp
+++ Define.hpp
@@ -4,7 +4,11 @@
 #define VECTOR  std::vector
 #define MAP     std::map
 #define STRING  std::string
+#define COUT    std::cout
+
 #define CRLF    "\r\n"
 #define LF      "\n"
+#define BUFFER_MAX  1<<10
+#define SOCKET  long
 
 #endif
