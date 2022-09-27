b02dce382f5e0747145ebacbc6504dce2fed0c2d
42hena <somnium1108@gmail.com>
2022-09-27 17:39:02 +0900

    Add :
    -  more files

--- Define.hpp
+++ Define.hpp
@@ -1,6 +1,10 @@
 #ifndef __DEFINE_HPP__
 #define __DEFINE_HPP__
 
+#include <map>
+#include <vector>
+#include <string>
+
 #define VECTOR  std::vector
 #define MAP     std::map
 #define STRING  std::string
