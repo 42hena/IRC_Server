e755c36107aa60372091d1764861bf418bc3685a
42hena <somnium1108@gmail.com>
2023-09-05 19:49:55 +0900

    [Feat] 서버 클래스의 포트와 패스워드 설정 구현
    - port의 getter와 setter를 개발
    - password이 getter와 setter를 개발

--- IRC_server.h
+++ IRC_server.h
@@ -16,7 +16,13 @@ private:
 	bool IsArgvValid(char* argv[]);
 	void ChangeServerDown();
 	bool IsPortValid(int ret_atoi);
-	
+
+	unsigned short GetServPort();
+	void SetServPort(int port);
+
+	std::string GetServPassword();
+	void SetServPassword(char* inputPassword);
+
 	bool				status;
 	unsigned short		port;
 	std::string			password;
